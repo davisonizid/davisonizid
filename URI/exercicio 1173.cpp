0da97093a2474ecc0a033596f30dcac7021d79da
davison <davisonizid@gmail.com>
2021-11-06 13:54:01 -0300

    teste

---
+++ URI/exercicio 1173.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x;
+        cin >> x
+
+    int vet [10];
+    vet[0] = x;
+
+    for (int  = 1; i <1 0; i++){
+        vet[i] = vet [i-1] * 2;
+        }
+
+    for (int i = 0; 1 < 10; i++){
+        cout << "N[" << i << "] = " << vet[i] << endl;
+        }
+}
