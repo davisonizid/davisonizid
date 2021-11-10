7f7efa770e71534c5d1c703242ad0852e8ac580a
davison <davisonizid@gmail.com>
2021-11-10 10:42:53 -0300

    OK

---
+++ IntroducaoJavascript/js/main.js
@@ -0,0 +1,10 @@
+
+var nome = "Davison Izidoro";
+var n1 = 29;
+var idade2 = 10;
+var frase = "Japão melhor time do mundo"; //troca no console
+//alert (" Bem Vindo " + nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + idade2);
+console.log(frase.replace("Japão","Brasil"));
--- URI/exercicio1174.cpp
+++ URI/exercicio1174.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 
 int main(){
+
+    float vet[100];
     
+    for (int i = 0; i < 100; i++){
+    cin >> vet [i];
+    }
+    
+    std::cout << std::setprecision(1) << std::fixed;
+    
+    for (int i = 0; i < 100; i++){
+        if (vet[i] <=10){
+    cout << "A[" << i << "] = " << vet[i] << endl;
+        }
+    }
 }
